dd23c6eddfdc4827ab6bd4bb05e3c14332bfd40f
LF56 <leah.fox7@gmail.com>
2022-04-30 08:44:35 -0400

    added reaction schema in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,30 @@
 const { Schema, model } = require('mongoose');
 
+
+//reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: "Reaction required to continue",
+            maxlength: [280, "reaction must not exceed 280 characters"],
+        },
+        username: {
+            type: String,
+            required: "Username required to proceed",
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+    }
+)
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -35,4 +60,4 @@ ThoughtSchema.virtual('reactionCount').get(function () {
 const Thought = model('Thought', ThoughtSchema);
 
 //export the model
-module.exports = { Thought }
+module.exports = Thought;
